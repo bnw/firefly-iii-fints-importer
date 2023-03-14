6a64655a636b4aaffff0a0052b71760a81a9f0bc
Daniel Funke <funke@kit.edu>
2023-03-14 10:47:40 +0100

    add tests for regex

--- tests/TransactionsToFireflySenderTest.php
+++ tests/TransactionsToFireflySenderTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\ConfigurationFactory;
 use App\TransactionsToFireflySender;
 use Fhp\Model\StatementOfAccount\Transaction;
 use PHPUnit\Framework\TestCase;
@@ -90,6 +91,52 @@ final class TransactionsToFireflySenderTest extends TestCase
         $actual   = TransactionsToFireflySender::transform_transaction_to_firefly_request_body(
             $transaction,
             $this->firefly_account_id, "", ""
+        );
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_transaction_processing_debit_regex()
+    {
+        $transaction = new Transaction;
+        $transaction->setAccountNumber($this->valid_iban);
+        $transaction->setCreditDebit(Transaction::CD_DEBIT);
+        $transaction->setValutaDate(new DateTime('2020-06-01'));
+        $transaction->setAmount(3.14);
+        $transaction->setName('destination_name');
+        $transaction->setStructuredDescription(array('SVWZ' => 'LASTSCHRIFT / BELASTUNGExample StoreKARTE 000000000KDN-REF 000000Ref. XXXX/0000', 'ABWA' => 'bakery'));
+
+        $configuration = ConfigurationFactory::load_from_file('data/configurations/example.json');
+
+        $exp_regex_match = '/^(Übertrag \/ Überweisung|Lastschrift \/ Belastung)(.*)(END-TO-END-REF.*|Karte.*|KFN.*)(Ref\..*)$/mi';
+        $exp_regex_replace = '$2 [$1 | $3 | $4]';
+
+        $this->assertEquals($exp_regex_match, $configuration->description_regex_match);
+        $this->assertEquals($exp_regex_replace, $configuration->description_regex_replace);
+
+        $expected = array(
+            'apply_rules' => true,
+            'error_if_duplicate_hash' => true,
+            'transactions' => array(
+                array(
+                    'type' => 'withdrawal',
+                    'date' => '2020-06-01',
+                    'amount' => 3.14,
+                    'description' => 'Example Store [LASTSCHRIFT / BELASTUNG | KARTE 000000000KDN-REF 000000 | Ref. XXXX/0000]',
+                    'source_name' => null,
+                    'source_id' => $this->firefly_account_id,
+                    'source_iban' => null,
+                    'destination_name' => 'destination_name',
+                    'destination_id' => null,
+                    'destination_iban' => $this->valid_iban,
+                    'sepa_ct_id' => '',
+                    'notes' => 'bakery',
+                )
+            )
+        );
+        $actual   = TransactionsToFireflySender::transform_transaction_to_firefly_request_body(
+            $transaction,
+            $this->firefly_account_id, $configuration->description_regex_match, $configuration->description_regex_replace
         );
 
         $this->assertEquals($expected, $actual);
