a757a3981a9c31ee9502110a8a48b0e9815ae36d
Tyrion <contact@christianwirth.net>
2023-10-27 22:36:40 +0200

    apply key

--- app/CollectData.php
+++ app/CollectData.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 function CollectData()
 {
-    global $request, $session, $twig, $automate_without_js;
+    global $request, $session, $twig, $automate_without_js, $encryption_key;
 
     if($request->request->get('data_collect_mode') == "createNewDataset"){
         echo $twig->render(
@@ -28,7 +28,16 @@ function CollectData()
         } 
         if ($request->request->has('bank_password')) {
             $configuration->bank_password = $request->request->get('bank_password');
-        }          
+        }      
+
+        if (!empty($configuration->nonce)) {
+            $key = hex2bin($request->request->get('key'));
+            $nonce = hex2bin($configuration->nonce);
+            $encrypted_pwd = hex2bin($configuration->bank_password);
+            
+            $configuration->bank_password = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt($encrypted_pwd, '', $nonce, $key);
+        }
+        
         if ($configuration->bank_username == "" || $configuration->bank_password == "") {
             echo $twig->render(
                 'collecting-data.twig',
@@ -39,7 +48,7 @@ function CollectData()
                 ));
             return;
         }
-
+            
         $session->set('bank_username',           $configuration->bank_username);
         $session->set('bank_password',           $configuration->bank_password);
         $session->set('bank_url',                $configuration->bank_url);
